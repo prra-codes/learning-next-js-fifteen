6f846cda1f486f560d1c1b138f0a12ae7d64cb26
peter <peter.arth5@gmail.com>
2025-09-02 16:46:49 +0100

    Learnt more of Next.js, including about layouts, links and loading ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Welcome home!</h1>;
+  return (
+    <>
+      <h1>Welcome home!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+      <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+    </>
+  );
 }
 
 // page.tsx file inside the app folder automatically maps your site's root URL
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 export default async function ProductReview({
   params,
@@ -8,7 +8,8 @@ export default async function ProductReview({
   const { productId, reviewId } = await params;
 
   if (parseInt(reviewId) > 1000) {
-    notFound();
+    // notFound();
+    redirect('/products');
   }
 
   return (
